7ac44a6353df7c1d2464d0b3fa911a5579ac7f79
JulienDegermann <degermann.julien@gmail.com>
2024-08-12 16:44:55 +0200

    add controls in js (mimetype and weight)

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -2,7 +2,30 @@ const formInput = document.querySelector('#files');
 const form = document.querySelector('form');
 
 formInput.addEventListener('change', () => {
-  form.submit()
+  let totalWeight = 0
+  let error = ''
+
+  Array.from(formInput.files).forEach(file => {
+    error = file.type !== 'application/pdf' && file.type !== 'image/jpeg' ? 'Un fichier n\'est pas au bon format (JPEG ou PDF)' : ''
+    totalWeight += file.size
+  })
+
+  // stop if file not pdf or jpeg
+  if (error !== '') {
+    alert(error)
+    return
+  }
+
+  error += totalWeight > 8388608 ? 'Le poids total des fichiers ne doit pas dépasser 8 MO' : ''
+  // stop if total weight > 8 Mo (can be changed with server config)
+  if (error !== '') {
+    alert(error)
+    return
+  }
+
+  if (error === '') {
+    form.submit()
+  }
 })
 
 const deleteButtons = document.querySelectorAll('.delete');
