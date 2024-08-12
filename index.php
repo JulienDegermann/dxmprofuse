5dd6bf3e2e3ae28791d5c2cf0d3c1e09dc92edc5
JulienDegermann <degermann.julien@gmail.com>
2024-08-12 14:30:19 +0200

    add font and fix font weight and size

--- index.php
+++ index.php
@@ -25,8 +25,7 @@ include_once './src/api/getFiles.php';
       <form action="src/api/addFiles.php" method="POST" enctype="multipart/form-data" class="flex">
         <?php include_once './src/assets/icons/download.svg'; ?>
         <label class="bold" for="files">Glisser & Déposer <br /> vos fichiers <br /> ici</label>
-        <!-- <input id="files" type="file" name="files[]" accept="image/jpeg, application/pdf" multiple> -->
-        <input id="files" type="file" name="files[]"  multiple>
+        <input id="files" type="file" name="files[]"  multiple aria-label="charger des fichiers">
       </form>
       <div class="files">
         <div class="scroll">
--- templates/_fileItem.php
+++ templates/_fileItem.php
@@ -2,9 +2,9 @@
 
   <div>
     <span class="bold"><?= $file['filename'] ?></span><br>
-    <span><?= $file['size']/(1024) > 1024 ? round($file['size']/(1024 * 1024), 2) . ' Mb' : floor($file['size']/(1024)) . ' Kb' ?> </span>
+    <span><?= $file['size']/(1024) > 1024 ? round($file['size']/pow(1024, 2), 2) . ' MO' : floor($file['size']/(1024)) . ' KO' ?> </span>
   </div>
-  <button class="delete" id="<?= $file['id'] ?>">
+  <button class="delete" id="<?= $file['id'] ?>" aria-label="supprimer le fichier">
     <?php include './src/assets/icons/trash.svg'; ?>
   </button>
   </button>
